a5890d3b942c90432a8664b90b8600fbbf62be53
bya <byambaa.mng@gmail.com>
2023-12-11 22:45:57 -0800

    FAB for new recipe added!

--- app/src/main/java/com/example/foodapp/fragment/RecipesFragment.kt
+++ app/src/main/java/com/example/foodapp/fragment/RecipesFragment.kt
@@ -1,14 +1,20 @@
 package com.example.foodapp.fragment
 
+import android.app.AlertDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.foodapp.R
 import com.example.foodapp.adapter.RecipeAdapter
 import com.example.foodapp.data.Recipe
 import com.example.foodapp.databinding.FragmentRecipesBinding
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class RecipesFragment: Fragment() {
     private lateinit var binding: FragmentRecipesBinding
@@ -93,5 +99,39 @@ class RecipesFragment: Fragment() {
         val recipeAdapter = RecipeAdapter(recipes)
         binding.recViewRecipes.adapter = recipeAdapter
         binding.recViewRecipes.layoutManager = LinearLayoutManager(view.context)
+
+        // Add recipe button
+        val fabNewRecipe: FloatingActionButton = binding.fabAddRecipe
+        fabNewRecipe.setOnClickListener {
+            showAddRecipeDialog()
+        }
+    }
+
+    private fun showAddRecipeDialog() {
+        val dialogView = LayoutInflater.from(requireContext()).inflate(R.layout.dialog_add_recipe, null)
+        val builder = AlertDialog.Builder(requireContext())
+        builder.setView(dialogView)
+
+        val dialog = builder.create()
+
+        val titleEditText = dialogView.findViewById<EditText>(R.id.editTextRecipeTitle)
+        val ingredientsEditText = dialogView.findViewById<EditText>(R.id.editTextIngredients)
+        val instructionsEditText = dialogView.findViewById<EditText>(R.id.editTextInstructions)
+        val addButton = dialogView.findViewById<Button>(R.id.buttonAddRecipe)
+
+        addButton.setOnClickListener {
+            val newRecipe = Recipe(
+                id = recipes.size + 1,
+                name = titleEditText.text.toString(),
+                ingredients = ingredientsEditText.text.toString(),
+                instructions = instructionsEditText.text.toString()
+            )
+            recipes.add(newRecipe)
+            binding.recViewRecipes.adapter?.notifyDataSetChanged()
+            Toast.makeText(this.context, "New Recipe '${newRecipe.name}' is added", Toast.LENGTH_SHORT).show()
+            dialog.dismiss()
+        }
+
+        dialog.show()
     }
 }
