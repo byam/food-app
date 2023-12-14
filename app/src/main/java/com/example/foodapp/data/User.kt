5c6fe79c1e23bda105b017d30c0b58000cc420df
bya <byambaa.mng@gmail.com>
2023-12-13 20:49:24 -0800

    Edit for about me

--- app/src/main/java/com/example/foodapp/data/User.kt
+++ app/src/main/java/com/example/foodapp/data/User.kt
@@ -2,10 +2,10 @@ package com.example.foodapp.data
 
 data class User(
     val id: Int,
-    val name: String, // e.g., "Harry Potter"
-    val email: String, // e.g., "harry@gmail.com"
+    var name: String, // e.g., "Harry Potter"
+    var email: String, // e.g., "harry@gmail.com"
     val password: String, // e.g., "h123123"
-    val phone: String, // e.g., "16122339211"
+    var phone: String, // e.g., "16122339211"
     val imageUri: Int, // e.g., R.drawable.ic_avatar_male
-    val bio: String = "" // Default value is an empty string
+    var bio: String = "" // Default value is an empty string
 )
--- app/src/main/java/com/example/foodapp/fragment/AboutMeFragment.kt
+++ app/src/main/java/com/example/foodapp/fragment/AboutMeFragment.kt
@@ -1,11 +1,16 @@
 package com.example.foodapp.fragment
 
 import UserPreferences
+import android.app.AlertDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import com.example.foodapp.R
 import com.example.foodapp.databinding.FragmentAboutMeBinding
 
 class AboutMeFragment: Fragment(){
@@ -35,5 +40,59 @@ class AboutMeFragment: Fragment(){
         binding.meBlogsName.text = "Blogs: ${ blogs.joinToString(separator = ", ") { it.title } }"
         binding.mePlanDates.text = "Meal Plan dates: ${ plans.joinToString(separator = ", ") { it.date } }"
         binding.meBio.text = currentUser.bio
+
+        // edit button
+        binding.meBtnEdit.setOnClickListener {
+            showEditDialog()
+        }
+    }
+
+    private fun showEditDialog() {
+        val dialogView = LayoutInflater.from(requireContext()).inflate(R.layout.dialog_edit_user, null)
+        val dialogBuilder = AlertDialog.Builder(requireContext())
+            .setView(dialogView)
+            .setTitle("Edit Profile")
+
+        val alertDialog = dialogBuilder.create()
+
+        val userPreferences = UserPreferences(requireContext())
+        val currentUser = userPreferences.getCurrentUser()
+
+        // Use findViewById to reference the EditTexts in the dialog
+        val editName = dialogView.findViewById<EditText>(R.id.edit_name)
+        val editEmail = dialogView.findViewById<EditText>(R.id.edit_email)
+        val editPhone = dialogView.findViewById<EditText>(R.id.edit_phone)
+        val editBio = dialogView.findViewById<EditText>(R.id.edit_bio)
+
+        // Pre-fill the EditTexts with current user data
+        editName.setText(currentUser?.name)
+        editEmail.setText(currentUser?.email)
+        editPhone.setText(currentUser?.phone)
+        editBio.setText(currentUser?.bio)
+
+        // Handle the save button click
+        dialogView.findViewById<Button>(R.id.edit_save_button).setOnClickListener {
+            // Update currentUser object with new values
+            currentUser?.let { user ->
+                user.name = editName.text.toString()
+                user.email = editEmail.text.toString()
+                user.phone = editPhone.text.toString()
+                user.bio = editBio.text.toString()
+
+                // Save updated user
+                userPreferences.saveCurrentUser(user)
+                // Update UI in fragment
+                updateUI()
+                alertDialog.dismiss()
+            }
+        }
+
+        alertDialog.show()
+    }
+
+    private fun updateUI() {
+        val currentUser = UserPreferences(requireContext()).getCurrentUser()
+        binding.meName.text = currentUser?.name
+        binding.meBio.text = currentUser?.bio
     }
 }
